bac67c3a1d6f7b7074e29cb21103929182424b11
Skillz4Killz <admin@ezlgg.com>
2017-06-29 18:48:10 -0400

    completed first scrape to get all 8 urls of shirts

--- scraper.js
+++ scraper.js
@@ -1,24 +1,61 @@
-var scrape = require('website-scraper');
-var options = {
-  urls: ['http://shirts4mike.com/'],
-  directory: './data/',
-};
+// http://shirts4mike.com/          http://shirts4mike.com/shirts.php
+'use strict';
+
+const cheerio = require("cheerio");
+const tinyreq = require("tinyreq");
+const scrapeIt = require("scrape-it");
+const url = "http://shirts4mike.com/shirts.php";
+const urlMain = "http://shirts4mike.com/";
+let pages = [];
+let page1, page2, page3, page4, page5, page6, page7, page8;
+
+// Define the scrape function
+function scrape(url, data, cb) {
+    // 1. Create the request
+    tinyreq(url, (err, body) => {
+        if (err) { return cb(err); }
+
+        // 2. Parse the HTML
+        let $ = cheerio.load(body)
+          , pageData = {}
+          ;
+
+        // 3. Extract the data
+        Object.keys(data).forEach(k => {
+            pageData[k] = $(data[k]).text();
+        });
+
+        // Send the data in the callback
+        cb(null, pageData);
+    });
+}
  
-// with promise 
-scrape({
-	urls: [{'http://shirts4mike.com/', filename: index.html},
-			{'http://shirts4mike.com/shirts.php', filename: shirts.html}],
-	directory: './data/',
-	sources:[
-				{selector: 'img', attr: 'src'}
-			]
-}).then((result) => {
-    /* some code here */
-}).catch((err) => {
-    /* some code here */
+// Promise interface to scrape the links for each shirt and create urls for each 
+scrapeIt(url, {
+	pageLinks: {
+        listItem: ".products li",
+        data: {
+            links: {
+                selector: "a",
+                attr: "href"
+            }
+        }
+    }
+}).then(data => { 
+	let shirtLinks = data;
+	shirtLinks.pageLinks.forEach(function(element) {
+		pages.push(element.links);
+	})
+	page1 = `${urlMain}${pages[0]}`;
+	page2 = `${urlMain}${pages[1]}`;
+	page3 = `${urlMain}${pages[2]}`;
+	page4 = `${urlMain}${pages[3]}`;
+	page5 = `${urlMain}${pages[4]}`;
+	page6 = `${urlMain}${pages[5]}`;
+	page7 = `${urlMain}${pages[6]}`;
+	page8 = `${urlMain}${pages[7]}`;
 });
+
+//scrape the necessary info from each url
+
  
-// or with callback 
-scrape(options, (error, result) => {
-    /* some code here */
-});
