6e3d30b3f552607ef6dc7593f5f25159ff6a88e4
Skillz <admin@ezlgg.com>
2017-06-29 23:26:13 -0400

    finished scraping all data now preparing to convert to csv but hit a bug

--- scraper.js
+++ scraper.js
@@ -4,31 +4,15 @@
 const cheerio = require("cheerio");
 const tinyreq = require("tinyreq");
 const scrapeIt = require("scrape-it");
+const jsoncsv = require('json-csv');
+const fs = require("fs");
+const es = require("event-stream");
 const url = "http://shirts4mike.com/shirts.php";
 const urlMain = "http://shirts4mike.com/";
+let date = new Date();
 let pages = [];
 let page1, page2, page3, page4, page5, page6, page7, page8;
-
-// Define the scrape function
-function scrape(url, data, cb) {
-    // 1. Create the request
-    tinyreq(url, (err, body) => {
-        if (err) { return cb(err); }
-
-        // 2. Parse the HTML
-        let $ = cheerio.load(body)
-          , pageData = {}
-          ;
-
-        // 3. Extract the data
-        Object.keys(data).forEach(k => {
-            pageData[k] = $(data[k]).text();
-        });
-
-        // Send the data in the callback
-        cb(null, pageData);
-    });
-}
+let shirtInfo = [];
  
 // Promise interface to scrape the links for each shirt and create urls for each 
 scrapeIt(url, {
@@ -54,8 +38,67 @@ scrapeIt(url, {
 	page6 = `${urlMain}${pages[5]}`;
 	page7 = `${urlMain}${pages[6]}`;
 	page8 = `${urlMain}${pages[7]}`;
+	pages = [page1, page2, page3, page4, page5, page6, page7, page8];
+
+		pages.forEach(function(element){
+			scrapeIt(element, {
+				title: {
+		      selector: "title"
+		    },
+		    price: {
+			      selector: ".price"
+			    },
+		    image: {
+		      selector: "img",
+		      attr: "src"
+		  	},
+			}).then(data => { 
+				shirtInfo = data;
+				shirtInfo.url = element;
+				shirtInfo.image = `${urlMain}${shirtInfo.image}`;
+				shirtInfo.time = new Date();
+				
+				shirtInfo = [shirtInfo];
+
+console.log(shirtInfo);
+
+			var options = {
+  fields : [
+		{
+        name : 'title',
+        label : 'Title',
+    },
+    {
+        name : 'price',
+        label : 'Price'
+    },
+    {
+        name : 'image',
+        label : 'ImageURL'
+    },
+    {
+        name : 'url',
+        label : 'URL'
+    },
+    {
+        name : 'time',
+        label : 'Time'
+    }]
+  }
+
+var out = fs.createWriteStream(`${date.getFullYear().toString()}-${(date.getMonth()+1).toString()}-${date.getDate().toString()}.csv`, {encoding: 'utf8'})
+var readable = es.readArray(shirtInfo)
+readable
+  .pipe(jsoncsv.csv(options))
+  .pipe(out)
+
+
+		});
+	})
 });
 
-//scrape the necessary info from each url
+
+
+			
 
  
